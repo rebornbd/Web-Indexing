46568838220bb7fa7a0c140eed61ae5c01963390
rebornbd <d.dipta.cse@gmail.com>
2021-01-30 14:15:54 +0600

    tasks + urls

--- indexing/models.py
+++ indexing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class WebIndexing(models.Model):
+  title       = models.CharField(max_length=250, blank=True, null=True)
+  description = models.TextField(blank=True, null=True)
+
+  link        = models.CharField(max_length=250)
+  parse_links = models.TextField(blank=True, null=True)
+
+  def __str__(self):
+    return self.title[:30]
---
+++ indexing/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# template tagging
+app_name = 'web_indexing'
+
+urlpatterns = [
+    # path('', views.WebIndexingListView, name='list'),
+]
